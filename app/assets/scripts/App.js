4fdb059c7ba1a5be2ec984731b4495345e4b5dc7
VincentMughal <vincentmarmalade@gmail.com>
2022-01-13 16:23:42 -0600

    Completed JavaScript for website.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,25 @@
 import '../styles/styles.css'
+import StickyHeader from './modules/StickyHeader.js'
+import MobileMenu from './modules/MobileMenu.js'
+
+new StickyHeader()
+new MobileMenu()
+
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if(typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal"*/'./modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem with the modal."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
     module.hot.accept();
